5f59b21a3874d3efecb927a0fbe38928467c8937
faishalfhid <faishalfh1.work@gmail.com>
2026-07-23 08:46:34 +0700

    Update env

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://bfznyxfjuhiymcnkfcga.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmem55eGZqdWhpeW1jbmtmY2dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MDEzMDMsImV4cCI6MjEwMDI3NzMwM30.4qUC_kLPs8Fylyz9FtCntmKZlhSddIpaw4_zr8AuJ6A'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
